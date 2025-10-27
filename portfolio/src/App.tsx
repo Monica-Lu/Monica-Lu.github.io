af34a1e05128c804553f5a7dd88b3ca2889ef4d2
monicalu <monicalu1128@hotmail.com>
2025-10-27 00:34:59 -0400

    Update App.tsx

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -44,8 +44,8 @@
 // }
 
 import { useState } from 'react'
-import reactLogo from '../react.svg'
-import viteLogo from '../vite.svg'
+import reactLogo from '../public/react.svg'
+import viteLogo from '../public/vite.svg'
 import './App.css'
 
 function App() {
