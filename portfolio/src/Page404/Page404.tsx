30b38c5ae675284c9a9b41501ce7771e96d1ce3a
Monica Lu <53923036+Monica-Lu@users.noreply.github.com>
2026-02-03 13:53:19 -0500

    Reapply "Second try on Page 404"
    This reverts commit 3f9953794d06bc7f8924413cf1ccbc51906a8135.

--- portfolio/src/Page404/Page404.tsx
+++ portfolio/src/Page404/Page404.tsx
@@ -1,16 +1,18 @@
 
 import { useNavigate } from 'react-router-dom';
 
-export default function NotFound(): React.JSX.Element {
+const NotFound = () => {
     const navigate = useNavigate();
 
     return (
         <div className=" ">
             <h1>404</h1>
             <p>Page not found.</p>
-            <button onClick={() => navigate('/')}>
-                Return to Home
+            <button onClick={() => navigate(-1)}>
+                Return to Previous Page
             </button>
         </div>
-  );
-}
+    );
+};
+
+export default NotFound;
